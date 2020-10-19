fcba76e192314a5d4babc65fb79371705acd821e
douglasdoucet <ddoucet@macalester.edu>
2020-10-19 07:42:24 -0700

    Worked on Block Spawner Script

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BlockSpawner : MonoBehaviour
 {
+    public GameObject rectBlockPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,20 @@ public class BlockSpawner : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("1");
+        if(other.transform.tag == "Hands")
+        {
+            Debug.Log("2");
+            Vector3 handPos = other.transform.position; 
+            if(Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.2f)
+            {
+                Debug.Log("3");
+                Instantiate(rectBlockPrefab, handPos, Quaternion.identity);
+            }
+        }
     }
 }
