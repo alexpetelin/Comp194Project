e76317f677ec634ed29a587c1b54e8369e3dec7c
amotoyam2024 <70379561+amotoyam2024@users.noreply.github.com>
2020-10-20 09:32:08 -0500

    Text testing

---
+++ Assets/Scripts/DisplayRandomText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayRandomText : MonoBehaviour
+{
+    public TextMeshPro _Text;
+    private string prompt = "prompt 1";
+
+    void Start()
+    {
+        _Text = gameObject.AddComponent<TextMeshPro>();
+        _Text.text = prompt;
+        
+        //Text = FindObjectOfType<TextMeshPro>();
+    }
+
+
+    void Update()
+    {
+        //Text.text = "Prompt 1";
+    }
+}
