fb8b9641af848c89ebd6a66218329ba607071f67
alexpetelin <72766163+alexpetelin@users.noreply.github.com>
2020-10-19 20:00:04 -0500

    10/19 - went to office hours fixed some problems with the bucketcolors and trash scripts, added a trash can and gameobject destroy script

---
+++ Assets/Scriptss/Trashcan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashcan : MonoBehaviour
+{
+    public GameObject trashcanobject;
+
+    void OnCollisionEnter(Collision other) {
+        
+            Destroy(other.gameObject);
+        
+    }
+  
+}
