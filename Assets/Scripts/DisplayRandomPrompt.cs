344abfc01cc60e54fb9e77efc65d28ee657d7416
amotoyam2024 <70379561+amotoyam2024@users.noreply.github.com>
2020-10-20 10:10:23 -0500

    Text testing (adding randomizer)

---
+++ Assets/Scripts/DisplayRandomPrompt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class DisplayRandomText : MonoBehaviour
+{
+
+    public TMP_Text randomPrompt;
+    public string [ ] promptList = new string [ ]{"horse", "house", "tree", "robot"};
+    
+    
+    // public TextMeshPro _Text;
+    // private string prompt = "prompt 1";
+
+    void Start()
+    {
+        // _Text = gameObject.AddComponent<TextMeshPro>();
+        // _Text.text = prompt;
+        //Text = FindObjectOfType<TextMeshPro>();
+    }
+
+
+    void Update()
+    {
+        randomPrompt.text = "horse";
+        //Text.text = "Prompt 1";
+    }
+}
